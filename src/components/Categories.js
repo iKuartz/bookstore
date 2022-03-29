9c577ff8c3e7ac821260e915ac74e581c0885bad
Ivan Silva <ivankuartz@hotmail.com>
2022-03-29 18:28:09 -0300

    Under construction is now being logged in the action

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,21 @@
-const Categories = () => (
-  <section className="categories-section">
-    <h1 className="categories-progress">Under construction</h1>
-    <button type="button">Check Status</button>
-  </section>
-);
+import { useDispatch } from 'react-redux';
+import { statusCategories } from '../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  return (
+    <section className="categories-section">
+      <h1 className="categories-progress">Under construction</h1>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(statusCategories());
+        }}
+      >
+        Check Status
+      </button>
+    </section>
+  );
+};
 
 export default Categories;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -3,6 +3,7 @@ const VERIFY = 'VERIFY';
 export default function categoriesReducer(state = {}, action) {
   switch (action.type) {
     case VERIFY:
+      console.log(action);
       return 'Under construction';
     default:
       return state;
