388bce8509cd8930e8e456f7e6e3abe92cf7d352
Ivan Silva <ivankuartz@hotmail.com>
2022-03-29 18:32:23 -0300

    Under construction is now being logged in the action, and returned when clicking on the check status button. also removed console log introduced for testing

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -3,7 +3,6 @@ const VERIFY = 'VERIFY';
 export default function categoriesReducer(state = {}, action) {
   switch (action.type) {
     case VERIFY:
-      console.log(action);
       return 'Under construction';
     default:
       return state;
